88390f7183c0f495bd825ab7256fe52331597c10
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2017-08-19 22:37:50 -0400

    test moment

--- src/app/services/ipc.client.service.ts
+++ src/app/services/ipc.client.service.ts
@@ -1,29 +1,46 @@
-import { Injectable, NgZone } from '@angular/core';
-import * as electron from 'electron';
+import { Observable, Observer } from 'rxjs/Rx';
+import { Injectable } from '@angular/core';
+// import * as electron from 'electron';
 declare let window: any;
+declare let electron: any;
+
+export type IpcClientData = {
+  event: any,
+  data: any,
+}
 
 @Injectable()
 export class IpcClientService {
 
-  private ipc = window.require('electron').ipcRenderer;
+  // private ipc = window.require('electron').ipcRenderer;
+  private ipc = electron.ipcRenderer;
 
-  constructor(
-    private zone: NgZone
-  ) {
-  }
-
-  on(listener: string, cb: Function, updateZone = true) {
-    this.ipc.on(listener, (event, arg) => {
-      arg = JSON.parse(arg);
-      cb(event, arg);
-      if (updateZone) {
-        this.zone.run(() => { });
-      }
+  on(listener: string) {
+    return Observable.create((observer: Observer<IpcClientData>) => {
+      // observer.next({
+      //   event: '',
+      //   data: '',
+      // });
+      this.ipc.on(listener, (event, arg) => {
+        arg = JSON.parse(arg);
+        if (arg.error) {
+          observer.error(arg.error);
+        } else {
+          observer.next({
+            event: event,
+            data: arg.data,
+          })
+        }
+      });
     });
   }
 
-  send(method, arg: any = null) {
-    this.ipc.send(method, JSON.stringify(arg));
+  send(message, arg: any = null) {
+    let observable = Observable.create((observer: Observer<void>) => {
+      this.ipc.send(message, JSON.stringify(arg));
+      observer.complete();
+    })
+    return Observable.concat(observable, this.on(message));
   }
 
   sendSync(method: string, arg: any = null) {
