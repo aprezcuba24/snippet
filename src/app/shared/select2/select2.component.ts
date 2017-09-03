36e0c277ef3fd557f88260e2c5ffb06561b47099
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2017-09-03 15:11:21 -0400

    select por defecto

--- src/app/shared/select2/select2.component.ts
+++ src/app/shared/select2/select2.component.ts
@@ -1,69 +1,76 @@
-import {Component, forwardRef, Input, OnInit, ElementRef, ViewChild} from '@angular/core';
+import {Component, forwardRef, Input, OnInit, ElementRef, ViewChild, AfterContentInit} from '@angular/core';
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+import {Observable, BehaviorSubject} from "rxjs/Rx";
+import {TagInterface} from "../../domain_types";
 declare let $: any;
 
 const noop = (_?: any) => {
 };
 
 export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
-  provide: NG_VALUE_ACCESSOR,
-  useExisting: forwardRef(() => Select2Component),
-  multi: true
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => Select2Component),
+    multi: true
 };
 
 
 @Component({
-  selector: 'select2',
-  templateUrl: './select2.component.html',
-  styleUrls: ['./select2.component.css'],
-  providers: [CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR]
+    selector: 'select2',
+    templateUrl: './select2.component.html',
+    styleUrls: ['./select2.component.css'],
+    providers: [CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR]
 })
-export class Select2Component implements ControlValueAccessor, OnInit {
+export class Select2Component implements ControlValueAccessor, AfterContentInit {
 
-  private innerValue: any = [];
-  @ViewChild("selector") selector: ElementRef;
+    @ViewChild("selector") selector: ElementRef;
+    @Input() items: any[];
+    @Input() options: '{}';
+    @Input() required: boolean = false;
+    @Input() value_property: string = '_id';
+    @Input() text_property: string = 'name';
+    private innerValue$ = new BehaviorSubject([]);
+    private onTouchedCallback: () => void = noop;
+    private onChangeCallback: (_: any) => void = noop;
 
-  @Input() items: any[];
-  @Input() options: '{}';
-  @Input() required: boolean = false;
-  @Input() multiple: boolean = false;
-  @Input() value_property: string = '_id';
-  @Input() text_property: string = 'name';
+    get value(): any {
+        return this.innerValue$;
+    };
 
-  private onTouchedCallback: () => void = noop;
-  private onChangeCallback: (_: any) => void = noop;
-
-  get value(): any {
-    return this.innerValue;
-  };
-
-  set value(v: any) {
-    if (v !== this.innerValue) {
-      this.innerValue = v;
-      this.onChangeCallback(v);
+    set value(v: any) {
+        this.innerValue$.next(v);
+        this.onChangeCallback(v);
     }
-  }
 
-  ngOnInit() {
-    $(this.selector.nativeElement).select2(this.options);
-    let $eventSelect = $(this.selector.nativeElement);
-    $eventSelect.on("change", () => this.value = $(this.selector.nativeElement).val());
-  }
-
-  writeValue(value: any) {
-    value = value || [];
-    console.log(value)
-    if (value !== this.innerValue) {
-      this.innerValue = value;
+    ngAfterContentInit() {
+        Observable.of(null).delay(20)
+            .subscribe(() => {
+                $(this.selector.nativeElement).select2(this.options);
+                let $eventSelect = $(this.selector.nativeElement);
+                $eventSelect.on("change", () => this.value = $(this.selector.nativeElement).val());
+            })
+        ;
     }
-  }
 
-  registerOnChange(fn: any) {
-    this.onChangeCallback = fn;
-  }
+    isSelected$(tag: TagInterface) {
+        return this.innerValue$
+            .filter(items => items != null)
+            .flatMap(items => Observable.from(items))
+            .findIndex((item: TagInterface) => item._id == tag._id)
+            .map(index => index != -1)
+        ;
+    }
 
-  registerOnTouched(fn: any) {
-    this.onTouchedCallback = fn;
-  }
+    writeValue(value: any) {
+        value = value || [];
+        this.innerValue$.next(value);
+    }
+
+    registerOnChange(fn: any) {
+        this.onChangeCallback = fn;
+    }
+
+    registerOnTouched(fn: any) {
+        this.onTouchedCallback = fn;
+    }
 
 }
